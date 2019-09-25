0ad2ee525a28dd565e01a3150c328e3e3637646b
VladKostyukevich <vlad.kostyukevich@gmail.com>
2019-09-25 14:05:09 +0300

    Add suites

---
+++ tests/scenarios/uiTests/smoke/spec3.js
@@ -0,0 +1,32 @@
+const pageHelpers = require('../../../helpers/pageHelpers');
+const settings = require('../../../../settings');
+const asserts = require('../../../helpers/asserts');
+const homePage = require('../../../components/homePage');
+const homePageConstants = require('../../../constants/homePageConstants');
+
+describe('Suite 3:', () => {
+    it('Checking label in main navigation panel', async () => {
+        await pageHelpers.navigateToPage(settings.frontendLink);
+        await asserts.assertEquals(await homePage.getTextOfKitchenPricing(), homePageConstants.MAIN_NAVIGATION.KITCHEN_PRICING);
+    });
+
+    describe('Smoke Tests 3.1', () => {
+        it('Checking label in main navigation panel 2', async () => {
+            await pageHelpers.navigateToPage(settings.frontendLink);
+            await asserts.assertEquals(await homePage.getTextOfKitchenPricing(), homePageConstants.MAIN_NAVIGATION.KITCHEN_PRICING);
+        });
+
+        it('Pending Test', () => {
+            pending();
+        });
+
+        it('Failed Test', () => {
+            return asserts.assertEquals('This value', 'That value');
+        });
+
+        xit('Excluded', () => {
+            asserts.assertEquals('This value', 'This value');
+        });
+    });
+});
+
